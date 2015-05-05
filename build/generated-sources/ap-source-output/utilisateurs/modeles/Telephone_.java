0c702ba1334245bcc6e7a08828fb295dbbd5d328
Ben06 <compteplaync@gmail.com>
2015-05-05 19:01:25 +0200

    telephone et adresse

---
+++ build/generated-sources/ap-source-output/utilisateurs/modeles/Telephone_.java
@@ -0,0 +1,14 @@
+package utilisateurs.modeles;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-05-05T14:54:50")
+@StaticMetamodel(Telephone.class)
+public class Telephone_ { 
+
+    public static volatile SingularAttribute<Telephone, String> numero;
+    public static volatile SingularAttribute<Telephone, Integer> id;
+
+}
